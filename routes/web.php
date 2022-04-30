241558663a891fe2da9f43c9a6a81e5e4c59a0b3
Alex Mwangi <alex@stl-horizon.com>
2022-05-01 00:39:33 +0300

    Added route for edit/update

--- routes/web.php
+++ routes/web.php
@@ -39,4 +39,6 @@ Route::prefix('users')->group(function () {
     Route::get('index', [UserController::class, 'index'])->name('view.users');
     Route::get('create', [UserController::class, 'create'])->name('add.user');
     Route::post('store', [UserController::class, 'store'])->name('store.user');
+    Route::get('edit/{id}', [UserController::class, 'edit'])->name('edit.user');
+    Route::post('update/{id}', [UserController::class, 'update'])->name('update.user');
 });
