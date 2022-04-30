b898ea5e006b355238ef3c73d693a14972461d64
Alex Mwangi <alex@stl-horizon.com>
2022-05-01 00:50:43 +0300

    Added route for delete user

--- routes/web.php
+++ routes/web.php
@@ -35,10 +35,10 @@ Route::get('/admin/logout', [AdminController::class, 'logout'])->name('admin.log
 // User Management
 
 Route::prefix('users')->group(function () {
-
     Route::get('index', [UserController::class, 'index'])->name('view.users');
     Route::get('create', [UserController::class, 'create'])->name('add.user');
     Route::post('store', [UserController::class, 'store'])->name('store.user');
     Route::get('edit/{id}', [UserController::class, 'edit'])->name('edit.user');
     Route::post('update/{id}', [UserController::class, 'update'])->name('update.user');
+    Route::get('delete/{id}', [UserController::class, 'destroy'])->name('delete.user');
 });
