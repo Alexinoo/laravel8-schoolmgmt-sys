ab77e82735a4cbbb6823c3decc8228f919dbd921
Alex Mwangi <alex@stl-horizon.com>
2022-05-01 12:53:46 +0300

    Added student class controller and CRUD functionality

---
+++ app/Http/Controllers/Backend/Setup/StudentClassController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Setup;
+
+use App\Http\Controllers\Controller;
+use App\Models\StudentClass;
+use Illuminate\Http\Request;
+
+class StudentClassController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = StudentClass::all();
+        return view('Backend.Setup.Student_class.index', compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('Backend.Setup.Student_class.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => ['required', 'unique:student_classes,name']
+        ]);
+        $user = new StudentClass();
+        $user->name = $request->name;
+        $user->save();
+
+        $notification = array(
+            'message' => 'Student class Added Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('student_class.index')->with($notification);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data  = StudentClass::find($id);
+
+        return view('Backend.Setup.Student_class.edit', compact('data'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->validate([
+            'name' => ['required', 'unique:student_classes,name']
+        ]);
+        $user = StudentClass::find($id);
+        $user->name = $request->name;
+        $user->update();
+
+        $notification = array(
+            'message' => 'Student class Updated Successfully',
+            'alert-type' => 'info'
+        );
+
+        return redirect()->route('student_class.index')->with($notification);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = StudentClass::find($id);
+        $user->delete();
+
+        $notification = array(
+            'message' => 'Student class deleted Successfully',
+            'alert-type' => 'error'
+        );
+
+        return redirect()->route('student_class.index')->with($notification);
+    }
+}
