463bd2c3a04c4b94399fbf13200ff370eef8661d
Alex Mwangi <alex@stl-horizon.com>
2022-05-01 22:36:09 +0300

    Added relationship with STudentClass

--- app/Models/FeeAmount.php
+++ app/Models/FeeAmount.php
@@ -14,4 +14,8 @@ class FeeAmount extends Model
     {
         return $this->belongsTo(FeeCategory::class, 'fee_category_id', 'id');
     }
+    public function student_class()
+    {
+        return $this->belongsTo(StudentClass::class, 'class_id', 'id');
+    }
 }
