cda001d32f1792abceac329c5d016fa0c1fa9b86
Alex Mwangi <alex@stl-horizon.com>
2022-05-01 10:57:17 +0300

    No major changes

--- resources/views/Backend/Profile/index.blade.php
+++ resources/views/Backend/Profile/index.blade.php
@@ -18,35 +18,35 @@
 					<div class="widget-user-header bg-black" >
 					  <h3 class="widget-user-username">Username : {{ $user->name }}</h3>
 					  <h6 class="widget-user-desc">Role : {{ $user->user_type ==0 ? 'User' : 'Admin' }}</h6>
-                        <a href="{{route('add.user')}}" class="btn btn-rounded btn-success mb-5 float-right">Edit Profile</a>
+                        <a href="{{route('edit.profile')}}" class="btn btn-rounded btn-success mb-5 float-right">Edit Profile</a>
 					  <h6 class="widget-user-desc">Email : {{ $user->email }}</h6>
                        
 					</div>
 					<div class="widget-user-image">
-					  <img class="rounded-circle" src="{{asset('storage/'.$user->profile_photo_path)}}" alt="User Avatar">
+					  <img class="rounded-circle" src="{{ isset($user->image) ? asset('uploads/user_images/'.$user->image) : asset('uploads/no_image.jpg')}}" alt="User Avatar">
 					</div>
 					<div class="box-footer">
 					  <div class="row">
 						<div class="col-sm-4">
 						  <div class="description-block">
-							<h5 class="description-header">12K</h5>
-							<span class="description-text">FOLLOWERS</span>
+							<h5 class="description-header">Phone</h5>
+							<span class="description-text">{{$user->phone }}</span>
 						  </div>
 						  <!-- /.description-block -->
 						</div>
 						<!-- /.col -->
 						<div class="col-sm-4 br-1 bl-1">
 						  <div class="description-block">
-							<h5 class="description-header">550</h5>
-							<span class="description-text">FOLLOWERS</span>
+							<h5 class="description-header">Address</h5>
+							<span class="description-text">{{ $user->address }}</span>
 						  </div>
 						  <!-- /.description-block -->
 						</div>
 						<!-- /.col -->
 						<div class="col-sm-4">
 						  <div class="description-block">
-							<h5 class="description-header">158</h5>
-							<span class="description-text">TWEETS</span>
+							<h5 class="description-header">Gender</h5>
+							<span class="description-text">{{$user->gender}}</span>
 						  </div>
 						  <!-- /.description-block -->
 						</div>
