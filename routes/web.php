bdcce8a5489ffecd8e23cef5a638b90c7b225e5c
Alex Mwangi <alex@stl-horizon.com>
2022-05-01 13:19:22 +0300

    Added routes for Student year CRUD

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\Setup\StudentClassController;
+use App\Http\Controllers\Backend\Setup\StudentYearController;
 use App\Http\Controllers\Backend\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -58,10 +59,20 @@ Route::prefix('profile')->group(function () {
 
 // Setups Management
 Route::prefix('setups')->group(function () {
+
+    // Student Class
     Route::get('student-class/index', [StudentClassController::class, 'index'])->name('student_class.index');
     Route::get('student-class/create', [StudentClassController::class, 'create'])->name('student_class.create');
     Route::post('student-class/store', [StudentClassController::class, 'store'])->name('student_class.store');
     Route::get('student-class/edit/{id}', [StudentClassController::class, 'edit'])->name('student_class.edit');
     Route::post('student-class/update/{id}', [StudentClassController::class, 'update'])->name('student_class.update');
     Route::get('student-class/delete/{id}', [StudentClassController::class, 'destroy'])->name('student_class.delete');
+
+    // Student Year
+    Route::get('student-year/index', [StudentYearController::class, 'index'])->name('student_year.index');
+    Route::get('student-year/create', [StudentYearController::class, 'create'])->name('student_year.create');
+    Route::post('student-year/store', [StudentYearController::class, 'store'])->name('student_year.store');
+    Route::get('student-year/edit/{id}', [StudentYearController::class, 'edit'])->name('student_year.edit');
+    Route::post('student-year/update/{id}', [StudentYearController::class, 'update'])->name('student_year.update');
+    Route::get('student-year/delete/{id}', [StudentYearController::class, 'destroy'])->name('student_year.delete');
 });
