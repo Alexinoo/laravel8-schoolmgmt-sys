fd039fbdb8b0b12e02d6ae5f074f50cd1a6d853b
Alex Mwangi <alex@stl-horizon.com>
2022-05-01 20:47:38 +0300

    Added menu for Fee category amounts

--- resources/views/Admin/inc/sidebar.blade.php
+++ resources/views/Admin/inc/sidebar.blade.php
@@ -71,6 +71,7 @@
             <li><a href="{{ route('student_group.index') }}"><i class="ti-more"></i>Student group</a></li>
             <li><a href="{{ route('student_shift.index') }}"><i class="ti-more"></i>Student shift</a></li>
             <li><a href="{{ route('fee_category.index') }}"><i class="ti-more"></i>Fee Category</a></li>
+            <li><a href="{{ route('fee_category_amount.index') }}"><i class="ti-more"></i>Fee Category Amount</a></li>
         
           </ul>
         </li>
