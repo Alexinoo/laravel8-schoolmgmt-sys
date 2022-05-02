878d8d839ad24f60cfee319cc1686595e4a6af66
Alex Mwangi <alex@stl-horizon.com>
2022-05-02 21:08:33 +0300

    Added edit/update student details

--- app/Http/Controllers/Backend/Student/StudentRegistrationController.php
+++ app/Http/Controllers/Backend/Student/StudentRegistrationController.php
@@ -159,9 +159,16 @@ class StudentRegistrationController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($student_id)
     {
-        //
+        $data['years'] = StudentYear::all();
+        $data['classes'] = StudentClass::all();
+        $data['groups'] = StudentGroup::all();
+        $data['shifts'] = StudentShift::all();
+
+        $data['model'] = StudentRegistration::with(['user', 'discount'])->where('student_id', $student_id)->first();
+
+        return view('Backend.Student.Student_registration.edit', $data);
     }
 
     /**
@@ -171,9 +178,56 @@ class StudentRegistrationController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $student_id)
     {
-        //
+        $data = $request->validate([
+            'name' => 'required',
+        ]);
+
+        DB::transaction(function () use ($request, $student_id) {
+
+            $user = User::where('id', $student_id)->first();
+            $user->name = $request->name;
+            $user->father_name = $request->father_name;
+            $user->mother_name = $request->mother_name;
+            $user->phone = $request->phone;
+            $user->address = $request->address;
+            $user->gender = $request->gender;
+            $user->religion = $request->religion;
+            $user->dob = date('Y-m-d', strtotime($request->dob));
+
+            if ($request->hasfile('image')) {
+                $destination = 'uploads/student_images/' . $user->image;
+                if (File::exists($destination)) {
+                    File::delete($destination);
+                }
+                $file = $request->file('image');
+                $filename = time() . '.' . $file->getClientOriginalExtension();
+                $file->move('uploads/student_images/', $filename);
+                //Save the filename in the db
+                $user->image = $filename;
+            }
+            $user->save();
+
+            // Save to Student Registration
+            $student =  StudentRegistration::where('id', $request->id)->where('student_id', $student_id)->first();
+            $student->class_id = $request->class_id;
+            $student->year_id = $request->year_id;
+            $student->group_id = $request->group_id;
+            $student->shift_id = $request->shift_id;
+            $student->save();
+
+            // Save to school Discount
+            $discount = DiscountStudent::where('student_id', $student_id)->first();
+            $discount->discount = $request->discount;
+            $discount->save();
+        });
+        $notification = array(
+            'message' => 'Student registration inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('student_registration.index')->with($notification);
     }
 
     /**
@@ -182,8 +236,27 @@ class StudentRegistrationController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($student_id)
     {
         //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function search(Request $request)
+    {
+        $years = StudentYear::all();
+        $classes = StudentClass::all();
+
+        // search by year/class
+        $year_id = $request->year_id;
+        $class_id = $request->class_id;
+        $model = StudentRegistration::where('year_id', $year_id)->where('class_id', $class_id)->get();
+
+        return view('Backend.Student.Student_registration.index', compact('model', 'years', 'classes', 'year_id', 'class_id'));
     }
 }
