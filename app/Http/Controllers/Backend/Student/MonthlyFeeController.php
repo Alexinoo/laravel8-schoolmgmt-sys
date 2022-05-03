b6ad50393838fd2bcf8a9f23ce2764f01da29dac
Alex Mwangi <alex@stl-horizon.com>
2022-05-03 21:29:17 +0300

    Added Monthly fee slip and designed added school logo

---
+++ app/Http/Controllers/Backend/Student/MonthlyFeeController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Student;
+
+use App\Http\Controllers\Controller;
+use App\Models\FeeAmount;
+use App\Models\StudentClass;
+use App\Models\StudentRegistration;
+use App\Models\StudentYear;
+use Illuminate\Http\Request;
+use PDF;
+
+class MonthlyFeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['years'] = StudentYear::all();
+        $data['classes'] = StudentClass::all();
+
+        return view('Backend.Student.Monthly_fee.index', $data);
+    }
+
+
+
+    public function StudentMonthlyFeeClasswise(Request $request)
+    {
+        $year_id = $request->year_id;
+        $class_id = $request->class_id;
+
+        if ($year_id != '') {
+            $where[] = ['year_id', 'like', $year_id . '%'];
+        }
+        if ($class_id != '') {
+            $where[] = ['class_id', 'like', $class_id . '%'];
+        }
+        $allStudent = StudentRegistration::with(['discount'])->where($where)->get();
+
+        $html['thsource']  = '<th>SL</th>';
+        $html['thsource'] .= '<th>ID No</th>';
+        $html['thsource'] .= '<th>Student Name</th>';
+        $html['thsource'] .= '<th>Roll No</th>';
+        $html['thsource'] .= '<th>Monthly Fee</th>';
+        $html['thsource'] .= '<th>Discount </th>';
+        $html['thsource'] .= '<th>Student Fee </th>';
+        $html['thsource'] .= '<th>Action</th>';
+
+        foreach ($allStudent as $key => $value) {
+
+            $monthlyFee = FeeAmount::where('fee_category_id', 3)->where('class_id', $value->class_id)->first();
+
+            $color = 'success';
+            $html[$key]['tdsource']  = '<td>' . ($key + 1) . '</td>';
+            $html[$key]['tdsource'] .= '<td>' . $value->user->id_no . '</td>';
+            $html[$key]['tdsource'] .= '<td>' . $value->user->name . '</td>';
+            $html[$key]['tdsource'] .= '<td>' . $value->roll . '</td>';
+            $html[$key]['tdsource'] .= '<td>' . $monthlyFee->amount . '</td>';
+            $html[$key]['tdsource'] .= '<td>' . $value->discount->discount . '%' . '</td>';
+
+            $originalfee = $monthlyFee->amount;
+            $discount =  !empty($value->discount->discount) ? $value->discount->discount : 0;
+            $discounttablefee = $discount / 100 * $originalfee;
+            $finalfee = (float)$originalfee - (float)$discounttablefee;
+
+            $html[$key]['tdsource'] .= '<td>' . $finalfee . '$' . '</td>';
+            $html[$key]['tdsource'] .= '<td>';
+            $html[$key]['tdsource'] .= '<a class="btn btn-sm btn-' . $color . '" title="Monthly Fee Slip" target="_blanks" href="' . route("monthly_fee.slip") . '?class_id=' . $value->class_id . '&student_id=' . $value->student_id . '&month=' . $request->month . '"><i class="fa fa-file-pdf-o"></i></a>';
+            $html[$key]['tdsource'] .= '</td>';
+        }
+
+        return response()->json($html);
+    } // end method 
+
+
+    // Generate Monthly Fee Slip 
+    public function MonthlyFeeSlip(Request $request)
+    {
+        // Get student_id and class_id - passed to this route
+        $student_id = $request->student_id;
+        $class_id = $request->class_id;
+        $data['month'] = $request->month;
+
+        $data['model'] = StudentRegistration::with(['user', 'discount'])->where([
+            'student_id' => $student_id,
+            'class_id' => $class_id
+        ])->first();
+
+        $pdf = PDF::loadView('Backend.Student.Monthly_fee.monthly_feeslip_pdf', $data);
+
+        return $pdf->stream('MonthlyFee.pdf');
+    }
+}
---
+++ resources/views/Backend/Student/Monthly_fee/index.blade.php
@@ -0,0 +1,145 @@
+@extends('Admin.admin_master')
+
+@section('content')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+	  <div class="container-full">
+	
+		<!-- Main content -->
+		<section class="content">
+		  <div class="row">
+
+			<div class="col-12">
+			  <div class="box bb-3 border-warning">
+				  <div class="box-header">
+					<h4 class="box-title">Student <strong>Monthly Fee</strong></h4>
+				  </div>
+
+				  <div class="box-body">
+						<div class="row">
+							      <div class="col-md-3">
+                                     <div class="form-group">
+                                        <h5>Year</h5>
+                                        <div class="controls">
+                                          <select name="year_id" id="year_id"  class="form-control">
+                                              <option value="" selected='' disabled >Search By Year</option>
+                                              @foreach($years as $key => $year)
+                                                    <option value="{{$year->id}}">{{$year->name}}</option>
+                                              @endforeach                                            
+                                          </select>                                  
+                                        </div>
+                                    </div>
+                                </div>
+                               <div class="col-md-3">
+                                     <div class="form-group">
+                                        <h5>Class</h5>
+                                        <div class="controls">
+                                          <select name="class_id" id="class_id"  class="form-control">
+                                              <option value="" selected='' disabled >Search By Class</option>
+                                                @foreach($classes as $key => $class)
+                                                    <option value="{{$class->id}}">{{$class->name}}</option>
+                                              @endforeach     
+                                          </select>                                  
+                                        </div>
+                                    </div>
+                                </div>
+                               <div class="col-md-3">
+                                     <div class="form-group">
+                                        <h5>Month</h5>
+                                        <div class="controls">
+                                          <select name="month" id="month"  class="form-control">
+                                              <option value="" selected='' disabled >Search By Month</option>                                          
+                                                    <option value="Jan">Jan</option>
+                                                    <option value="Feb">Feb</option>
+                                                    <option value="Mar">Mar</option>
+                                                    <option value="Apr">Apr</option>
+                                                    <option value="May">May</option>
+                                                    <option value="Jun">Jun</option>
+                                                    <option value="Jul">Jul</option>
+                                                    <option value="Aug">Aug</option>
+                                                    <option value="Sep">Sep</option>
+                                                    <option value="Oct">Oct</option>
+                                                    <option value="Nov">Nov</option>
+                                                    <option value="Dec">Dec</option>
+                                         
+                                          </select>                                  
+                                        </div>
+                                    </div>
+                                </div>
+                               <div class="col-md-3" style="padding-top:25px;">
+                                    <a id="search" class="btn btn-primary" name="search">Search</a>
+                                </div>
+						</div>
+
+                {{-- ///////////////////////////// Monthly Fee  ///////////////////////// --}}
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div id="documentResults">
+                                    <script id="document-template" type="text/x-handlebars-template"> 
+                                     <table class="table table-bordered table-striped" style="width: 100%">
+                                            <thead>
+                                                  <tr>
+                                                @{{{thsource }}}
+                                                 </tr>
+                                            </thead>
+                                                <tbody>
+                                                  @{{#each this}}
+                                                        <tr>
+                                                            @{{{tdsource }}}
+                                                        </tr>
+                                                  @{{/each}}
+                                                </tbody>
+                                            </table>    
+                                        </script>
+                                    </div>
+                                </div>                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+		  </div>
+		  <!-- /.row -->
+		</section>
+		<!-- /.content -->
+	  
+	  </div>
+  </div>
+  <!-- /.content-wrapper -->
+
+@endsection
+
+
+
+{{-- ============ Get Registration Fee Method And View Page =================== --}}
+@section('scripts')
+<script type="text/javascript">
+  $(document).on('click','#search',function(){
+  
+     $.ajax({
+      url: "{{ route('Student-Monthly-Fee-Classwise')}}",
+      type: "GET",
+      data: {
+          year_id : $('#year_id').val(),
+          class_id : $('#class_id').val(),
+          month : $('#month').val()
+        },
+      beforeSend: function() {       
+      },
+      success: function (response) {
+        var source = $("#document-template").html();
+        var template = Handlebars.compile(source);
+        var html = template(response);
+        $('#documentResults').html(html);
+        $('[data-toggle="tooltip"]').tooltip();
+      }
+    });
+  });
+
+</script>
+@endsection
+
+{{-- ============ End Script Registration Fee ================= --}}
+
+    
