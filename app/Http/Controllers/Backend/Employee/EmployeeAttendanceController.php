196aa84fb807c8c8e0884d9c835e53aa45f17dd7
Alex Mwangi <alex@stl-horizon.com>
2022-05-05 22:55:48 +0300

    Added index() - show list of the attendance list

---
+++ app/Http/Controllers/Backend/Employee/EmployeeAttendanceController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Employee;
+
+use App\Http\Controllers\Controller;
+use App\Models\Employee_attendance;
+use App\Models\Employee_leave;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class EmployeeAttendanceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['model'] = Employee_attendance::orderBy('id', 'DESC')->get();
+
+        return view('Backend.Employee.Employee_attendance.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $data['employees'] = User::where('user_type', 'Employee')->get();
+
+        return view('Backend.Employee.Employee_attendance.create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
