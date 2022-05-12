7000703af7b7357cf54430151c6217438658131f
Alex Mwangi <alex@stl-horizon.com>
2022-05-12 10:42:40 +0300

    Added Monthly Salary menu

--- resources/views/Admin/inc/sidebar.blade.php
+++ resources/views/Admin/inc/sidebar.blade.php
@@ -1,158 +1,160 @@
   @php
-    $prefix = Request::route()->getPrefix();
-    $route = Route::current()->getName();
+  $prefix = Request::route()->getPrefix();
+  $route = Route::current()->getName();
   @endphp
 
   <!-- Left side column. contains the logo and sidebar -->
   <aside class="main-sidebar">
-    <!-- sidebar-->
-    <section class="sidebar">	
-		
-        <div class="user-profile">
-			<div class="ulogo">
-				 <a href="index.html">
-				  <!-- logo for regular state and mobile devices -->
-					 <div class="d-flex align-items-center justify-content-center">					 	
-						  <img src="{{ asset('backend/images/logo-dark.png') }}" alt="">
-						  <h3><b>Sunny</b> Admin</h3>
-					 </div>
-				</a>
-			</div>
-        </div>
-      
-      <!-- sidebar menu-->
-      <ul class="sidebar-menu" data-widget="tree">  
-		  
-		<li class="{{ ($route == 'dashboard') ? 'active': ''}}">
-          <a href="{{route('dashboard')}}">
-            <i data-feather="pie-chart"></i>
-			<span>Dashboard</span>
-          </a>
-        </li>  
-		
-        @if(Auth::user()->role == 'Admin')
-            <li class="treeview {{ ($prefix == '/users') ? 'active': ''}}">
-          <a href="#">
-            <i data-feather="message-circle"></i>
-            <span>Manage Users</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-right pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="{{ route('view.users')}}"><i class="ti-more"></i> Users</a></li>
-            <li><a href="{{route('add.user')}}"><i class="ti-more"></i>Add User</a></li>
+      <!-- sidebar-->
+      <section class="sidebar">
+
+          <div class="user-profile">
+              <div class="ulogo">
+                  <a href="index.html">
+                      <!-- logo for regular state and mobile devices -->
+                      <div class="d-flex align-items-center justify-content-center">
+                          <img src="{{ asset('backend/images/logo-dark.png') }}" alt="">
+                          <h3><b>Sunny</b> Admin</h3>
+                      </div>
+                  </a>
+              </div>
+          </div>
+
+          <!-- sidebar menu-->
+          <ul class="sidebar-menu" data-widget="tree">
+
+              <li class="{{ ($route == 'dashboard') ? 'active': ''}}">
+                  <a href="{{route('dashboard')}}">
+                      <i data-feather="pie-chart"></i>
+                      <span>Dashboard</span>
+                  </a>
+              </li>
+
+              @if(Auth::user()->role == 'Admin')
+              <li class="treeview {{ ($prefix == '/users') ? 'active': ''}}">
+                  <a href="#">
+                      <i data-feather="message-circle"></i>
+                      <span>Manage Users</span>
+                      <span class="pull-right-container">
+                          <i class="fa fa-angle-right pull-right"></i>
+                      </span>
+                  </a>
+                  <ul class="treeview-menu">
+                      <li><a href="{{ route('view.users')}}"><i class="ti-more"></i> Users</a></li>
+                      <li><a href="{{route('add.user')}}"><i class="ti-more"></i>Add User</a></li>
+                  </ul>
+              </li>
+              @endif
+
+
+              <li class="treeview {{ ($prefix == '/profile') ? 'active': ''}}">
+                  <a href="#">
+                      <i data-feather="mail"></i> <span>Manage Profile</span>
+                      <span class="pull-right-container">
+                          <i class="fa fa-angle-right pull-right"></i>
+                      </span>
+                  </a>
+                  <ul class="treeview-menu">
+                      <li><a href="{{ route('view.profile') }}"><i class="ti-more"></i>Your Profile</a></li>
+                      <li><a href="{{ route('view.password') }}"><i class="ti-more"></i>Change Password</a></li>
+                  </ul>
+              </li>
+
+
+              <li class="treeview {{ ($prefix == '/setups') ? 'active': ''}}">
+                  <a href="#">
+                      <i data-feather="mail"></i> <span>Setup Management</span>
+                      <span class="pull-right-container">
+                          <i class="fa fa-angle-right pull-right"></i>
+                      </span>
+                  </a>
+                  <ul class="treeview-menu">
+                      <li><a href="{{ route('student_class.index') }}"><i class="ti-more"></i>Student class</a></li>
+                      <li><a href="{{ route('student_year.index') }}"><i class="ti-more"></i>Student year</a></li>
+                      <li><a href="{{ route('student_group.index') }}"><i class="ti-more"></i>Student group</a></li>
+                      <li><a href="{{ route('student_shift.index') }}"><i class="ti-more"></i>Student shift</a></li>
+                      <li><a href="{{ route('fee_category.index') }}"><i class="ti-more"></i>Fee Category</a></li>
+                      <li><a href="{{ route('fee_category_amount.index') }}"><i class="ti-more"></i>Fee Category Amount</a></li>
+                      <li><a href="{{ route('exam_type.index') }}"><i class="ti-more"></i>Exam type</a></li>
+                      <li><a href="{{ route('school_subject.index') }}"><i class="ti-more"></i>School Subject</a></li>
+                      <li><a href="{{ route('assign_subject.index') }}"><i class="ti-more"></i>Assign Subject</a></li>
+                      <li><a href="{{ route('designation.index') }}"><i class="ti-more"></i>Designation</a></li>
+
+                  </ul>
+              </li>
+
+
+              <li class="treeview {{ ($prefix == '/students') ? 'active': ''}}">
+                  <a href="#">
+                      <i data-feather="mail"></i> <span>Student Management</span>
+                      <span class="pull-right-container">
+                          <i class="fa fa-angle-right pull-right"></i>
+                      </span>
+                  </a>
+                  <ul class="treeview-menu">
+                      <li><a href="{{ route('student_registration.index') }}"><i class="ti-more"></i>Student Registration</a></li>
+                      <li><a href="{{ route('generate_roll.index') }}"><i class="ti-more"></i>Roll Generate</a></li>
+                      <li><a href="{{ route('registration_fee.index') }}"><i class="ti-more"></i>Registration Fee</a></li>
+
+                      <li><a href="{{ route('monthly_fee.index') }}"><i class="ti-more"></i>Monthly Fee</a></li>
+
+                      <li><a href="{{ route('exam_fee.index') }}"><i class="ti-more"></i>Exam Fee</a></li>
+
+                  </ul>
+              </li>
+
+
+              <li class="treeview {{ ($prefix == '/employees') ? 'active': ''}}">
+                  <a href="#">
+                      <i data-feather="mail"></i> <span>Employee Management</span>
+                      <span class="pull-right-container">
+                          <i class="fa fa-angle-right pull-right"></i>
+                      </span>
+                  </a>
+                  <ul class="treeview-menu">
+                      <li><a href="{{ route('employee_registration.index') }}"><i class="ti-more"></i>Employee Registration</a></li>
+
+                      <li><a href="{{ route('employee_salary.index') }}"><i class="ti-more"></i>Employee Salary</a></li>
+
+                      <li><a href="{{ route('employee_leave.index') }}"><i class="ti-more"></i>Employee Leave</a></li>
+
+                      <li><a href="{{ route('employee_attendance.index') }}"><i class="ti-more"></i>Employee Attendance</a></li>
+
+                      <li><a href="{{ route('monthly_salary.index') }}"><i class="ti-more"></i>Monthly Salary</a></li>
+
+
+
+
+                  </ul>
+              </li>
+
+
+              <li class="header nav-small-cap">User Interface</li>
+
+              <li class="treeview">
+                  <a href="#">
+                      <i data-feather="grid"></i>
+                      <span>Components</span>
+                      <span class="pull-right-container">
+                          <i class="fa fa-angle-right pull-right"></i>
+                      </span>
+                  </a>
+                  <ul class="treeview-menu">
+                      <li><a href="components_alerts.html"><i class="ti-more"></i>Alerts</a></li>
+                      <li><a href="components_badges.html"><i class="ti-more"></i>Badge</a></li>
+                  </ul>
+              </li>
+
+
           </ul>
-        </li> 
-        @endif
-      
-		  
-        <li class="treeview {{ ($prefix == '/profile') ? 'active': ''}}">
-          <a href="#">
-            <i data-feather="mail"></i> <span>Manage Profile</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-right pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="{{ route('view.profile') }}"><i class="ti-more"></i>Your Profile</a></li>
-            <li><a href="{{ route('view.password') }}"><i class="ti-more"></i>Change Password</a></li>
-          </ul>
-        </li>
+      </section>
 
-
-        <li class="treeview {{ ($prefix == '/setups') ? 'active': ''}}">
-          <a href="#">
-            <i data-feather="mail"></i> <span>Setup Management</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-right pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="{{ route('student_class.index') }}"><i class="ti-more"></i>Student class</a></li>
-            <li><a href="{{ route('student_year.index') }}"><i class="ti-more"></i>Student year</a></li>
-            <li><a href="{{ route('student_group.index') }}"><i class="ti-more"></i>Student group</a></li>
-            <li><a href="{{ route('student_shift.index') }}"><i class="ti-more"></i>Student shift</a></li>
-            <li><a href="{{ route('fee_category.index') }}"><i class="ti-more"></i>Fee Category</a></li>
-            <li><a href="{{ route('fee_category_amount.index') }}"><i class="ti-more"></i>Fee Category Amount</a></li>
-            <li><a href="{{ route('exam_type.index') }}"><i class="ti-more"></i>Exam type</a></li>
-            <li><a href="{{ route('school_subject.index') }}"><i class="ti-more"></i>School Subject</a></li>
-            <li><a href="{{ route('assign_subject.index') }}"><i class="ti-more"></i>Assign Subject</a></li>
-            <li><a href="{{ route('designation.index') }}"><i class="ti-more"></i>Designation</a></li>
-        
-          </ul>
-        </li>
-
-
-        <li class="treeview {{ ($prefix == '/students') ? 'active': ''}}">
-          <a href="#">
-            <i data-feather="mail"></i> <span>Student Management</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-right pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="{{ route('student_registration.index') }}"><i class="ti-more"></i>Student Registration</a></li>
-            <li><a href="{{ route('generate_roll.index') }}"><i class="ti-more"></i>Roll Generate</a></li>
-            <li><a href="{{ route('registration_fee.index') }}"><i class="ti-more"></i>Registration Fee</a></li>
-
-            <li><a href="{{ route('monthly_fee.index') }}"><i class="ti-more"></i>Monthly Fee</a></li>
-
-            <li><a href="{{ route('exam_fee.index') }}"><i class="ti-more"></i>Exam Fee</a></li>
-           
-          </ul>
-        </li>
-
-          
-        <li class="treeview {{ ($prefix == '/employees') ? 'active': ''}}">
-          <a href="#">
-            <i data-feather="mail"></i> <span>Employee Management</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-right pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="{{ route('employee_registration.index') }}"><i class="ti-more"></i>Employee Registration</a></li>
-          
-            <li><a href="{{ route('employee_salary.index') }}"><i class="ti-more"></i>Employee Salary</a></li>
-
-            <li><a href="{{ route('employee_leave.index') }}"><i class="ti-more"></i>Employee Leave</a></li>
-
-            <li><a href="{{ route('employee_attendance.index') }}"><i class="ti-more"></i>Employee Attendance</a></li>
-
-          
-        
-           
-          </ul>
-        </li>
-		  
-		 
-        <li class="header nav-small-cap">User Interface</li>
-		  
-        <li class="treeview">
-          <a href="#">
-            <i data-feather="grid"></i>
-            <span>Components</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-right pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="components_alerts.html"><i class="ti-more"></i>Alerts</a></li>
-            <li><a href="components_badges.html"><i class="ti-more"></i>Badge</a></li>           
-          </ul>
-        </li>	
-
-        
-      </ul>
-    </section>
-	
-	<div class="sidebar-footer">
-		<!-- item-->
-		<a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Settings" aria-describedby="tooltip92529"><i class="ti-settings"></i></a>
-		<!-- item-->
-		<a href="mailbox_inbox.html" class="link" data-toggle="tooltip" title="" data-original-title="Email"><i class="ti-email"></i></a>
-		<!-- item-->
-		<a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Logout"><i class="ti-lock"></i></a>
-	</div>
+      <div class="sidebar-footer">
+          <!-- item-->
+          <a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Settings" aria-describedby="tooltip92529"><i class="ti-settings"></i></a>
+          <!-- item-->
+          <a href="mailbox_inbox.html" class="link" data-toggle="tooltip" title="" data-original-title="Email"><i class="ti-email"></i></a>
+          <!-- item-->
+          <a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Logout"><i class="ti-lock"></i></a>
+      </div>
   </aside>
