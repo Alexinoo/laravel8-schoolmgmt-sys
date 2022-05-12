d9e765458c48fe9737f7228c70028d278e3be364
Alex Mwangi <alex@stl-horizon.com>
2022-05-12 15:54:00 +0300

    Added rship with user table

--- app/Models/Student_mark.php
+++ app/Models/Student_mark.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Student_mark extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class , 'student_id' , 'id');
+    }
 }
