e12983f7069a3ce6574593c52e907523fed8c7a4
Alex Mwangi <alex@stl-horizon.com>
2022-05-12 15:53:19 +0300

    Added getStudentMarksForUpdate() - fetch student marks for update

--- app/Http/Controllers/Backend/DefaultController.php
+++ app/Http/Controllers/Backend/DefaultController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Models\AssignSubject;
+use App\Models\Student_mark;
 use App\Models\StudentRegistration;
 use Illuminate\Http\Request;
 
 class DefaultController extends Controller
 {
 
+    // Fetch Subjects and populate to Subject dropdown
     public function fetchSubjects(Request $request){
 
         $class_id = $request->class_id;
@@ -19,12 +21,30 @@ class DefaultController extends Controller
         return response()->json($data);
 
     }
+
+
+    // Populate students and the marks for entries
     public function getStudentMarks(Request $request){
 
         $year_id = $request->year_id;
         $class_id = $request->class_id;
 
         $data = StudentRegistration::with(['user'])->where('year_id', $year_id)->where('class_id' , $class_id)->get();
+
+        return response()->json($data);
+
+    }
+
+
+    // Get student marks Previously stored for update
+    public function UpdateStudentMarks(Request $request){
+
+        $year_id = $request->year_id;
+        $class_id = $request->class_id;
+        $subject_id = $request->subject_id;
+        $exam_type_id = $request->exam_type_id;
+
+        $data = Student_mark::with(['user'])->where('year_id', $year_id)->where('class_id' , $class_id)->where('subject_id', $subject_id)->where('exam_type_id', $exam_type_id)->get();
 
         return response()->json($data);
 
