70d755694b5760242d107068f09cd414e5950267
Alex Mwangi <alex@stl-horizon.com>
2022-05-12 16:34:32 +0300

    Added update logic - delete existing - insert afresh

--- app/Http/Controllers/Backend/Mark/MarkController.php
+++ app/Http/Controllers/Backend/Mark/MarkController.php
@@ -54,4 +54,51 @@ class MarkController extends Controller
         return redirect()->back()->with($notification);
 
     }
+
+    public function edit()
+    {
+
+        $data['years'] = StudentYear::all();
+        $data['classes'] = StudentClass::all();
+        $data['exam_types'] = ExamType::all();
+
+        return view('Backend.Mark.edit', $data);
+    }
+
+    public function update(Request $request)
+    {
+
+        DB::transaction(function () use ($request) {
+
+            // Delete
+            Student_mark::where('year_id',$request->year_id)->where('class_id', $request->class_id)->where('subject_id', $request->subject_id)->where('exam_type_id', $request->exam_type_id)->delete();
+
+            // Then insert
+            $cntStudents = $request->student_id;
+
+            if ($cntStudents) {
+                for ($i = 0; $i < count($request->student_id); $i++) {
+
+                    // Save data
+                    $model = new Student_mark;
+                    $model->student_id =  $request->student_id[$i];
+                    $model->id_no =  $request->id_no[$i];
+                    $model->year_id =  $request->year_id;
+                    $model->class_id =  $request->class_id;
+                    $model->subject_id =  $request->subject_id;
+                    $model->exam_type_id =  $request->exam_type_id;
+                    $model->marks =  $request->marks[$i];
+
+                    $model->save();
+                }
+            }
+        });
+
+        $notification = array(
+            'message' => 'Student Marks updated Successfully',
+            'alert-type' => 'info'
+        );
+
+        return redirect()->back()->with($notification);
+    }
 }
