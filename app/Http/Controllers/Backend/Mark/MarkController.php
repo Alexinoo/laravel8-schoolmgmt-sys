d778ca85b53a3b2e34b2061749a3274cebfa8771
Alex Mwangi <alex@stl-horizon.com>
2022-05-12 15:04:14 +0300

    Added store marks functionality in the store()

--- app/Http/Controllers/Backend/Mark/MarkController.php
+++ app/Http/Controllers/Backend/Mark/MarkController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Backend\Mark;
 
 use App\Http\Controllers\Controller;
 use App\Models\ExamType;
+use App\Models\Student_mark;
 use App\Models\StudentClass;
 use App\Models\StudentYear;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MarkController extends Controller
 {
@@ -17,6 +19,39 @@ class MarkController extends Controller
         $data['exam_types'] = ExamType::all();
 
         return view('Backend.Mark.entries' , $data);
+
+    }
+    public function store(Request $request){
+
+        DB::transaction(function() use ($request ){
+
+            $cntStudents = $request->student_id;
+
+            if ($cntStudents) {
+                for ($i = 0; $i < count($request->student_id); $i++) {
+
+                    // Save data
+                    $model = new Student_mark();
+                    $model->student_id =  $request->student_id[$i];
+                    $model->id_no =  $request->id_no[$i];
+                    $model->year_id =  $request->year_id;
+                    $model->class_id =  $request->class_id;
+                    $model->subject_id =  $request->subject_id;
+                    $model->exam_type_id =  $request->exam_type_id;
+                    $model->marks =  $request->marks[$i];
+
+                    $model->save();
+                }
+            }
+
+        });
+
+        $notification = array(
+            'message' => 'Student Marks inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
 
     }
 }
