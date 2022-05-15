29f56b38b2f2c8c6e265bac02c7886c62cfd5223
Alex Mwangi <alex@stl-horizon.com>
2022-05-15 17:05:59 +0300

    Added rship with Year/Class/Student/Exam_type classes

--- app/Models/Student_mark.php
+++ app/Models/Student_mark.php
@@ -12,4 +12,18 @@ class Student_mark extends Model
     public function user(){
         return $this->belongsTo(User::class , 'student_id' , 'id');
     }
+    public function year(){
+        return $this->belongsTo(StudentYear::class,'year_id','id');
+    }
+    public function class(){
+        return $this->belongsTo(StudentClass::class,'class_id','id');
+    }
+    public function student(){
+        return $this->belongsTo(StudentRegistration::class,'student_id','id');
+    }
+    public function exam(){
+        return $this->belongsTo(ExamType::class,'exam_type_id','id');
+    }
+
+    
 }
