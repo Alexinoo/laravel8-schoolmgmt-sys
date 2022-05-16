9ed542c1c6ac53b5d7fc40bc330f1fe68f7541cb
Alex Mwangi <alex@stl-horizon.com>
2022-05-16 15:03:20 +0300

    Added Dashboard icon

--- resources/views/Admin/inc/sidebar.blade.php
+++ resources/views/Admin/inc/sidebar.blade.php
@@ -26,6 +26,11 @@
               <li class="{{ ($route == 'dashboard') ? 'active': ''}}">
                   <a href="{{route('dashboard')}}">
                       <i data-feather="pie-chart"></i>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-pie-chart">
+                          <path d="M21.21 15.89A10 10 0 1 1 8 2.83"></path>
+                          <path d="M22 12A10 10 0 0 0 12 2v10z"></path>
+                      </svg>
+
                       <span>Dashboard</span>
                   </a>
               </li>
@@ -34,6 +39,7 @@
               <li class="treeview {{ ($prefix == '/users') ? 'active': ''}}">
                   <a href="#">
                       <i data-feather="message-circle"></i>
+
                       <span>Manage Users</span>
                       <span class="pull-right-container">
                           <i class="fa fa-angle-right pull-right"></i>
